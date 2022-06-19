e1bc8992144fcf1213f5bf3ee3431501ca624bea
DefenderYAY <ishu.defender@gmail.com>
2022-06-19 13:05:08 +0530

    some more slash commands

--- Commands.py
+++ Commands.py
@@ -76,6 +76,16 @@ async def help(ctx):
     helpEmbed.add_field(name = "IP Help [-serverIp]", value = "This tells you what servers we have and their ips!", inline = False)
     await ctx.send(embed = helpEmbed)
 
+@bot.slash_command(aliases = ["about", "what"], description = "These are the commands I know so far :D")
+async def help(ctx):
+    helpEmbed = discord.Embed(title = "Commands", description = "These are the commands I know so far :D", color = discord.Color.dark_orange())
+    helpEmbed.set_thumbnail(url="https://i.imgflip.com/60v6pm.jpg")
+    helpEmbed.add_field(name = "Rock Paper Scissors [-rps]", value = "This command allows you to play rock paper scissors against the bot! How to use the command: `-rps value` exchange the value with **rock paper or scissors**!", inline = False)
+    helpEmbed.add_field(name = "Coinflip [-coinflip]", value = "This command lets you flip a coin", inline = False)
+    helpEmbed.add_field(name = "Custom commands [-custom]", value = "This command show the list of custom commands for friends!")
+    helpEmbed.add_field(name = "IP Help [-serverIp]", value = "This tells you what servers we have and their ips!", inline = False)
+    await ctx.respond(embed = helpEmbed)
+
 #Cutsom Embed
 @bot.command(aliases = ["friend"])
 async def custom(ctx):
@@ -89,6 +99,19 @@ async def custom(ctx):
     customEmbed.add_field(name = "F O O T [-foot]", value = "F O O T", inline = False)
     customEmbed.add_field(name = "El Pepe's custom command [-elpepe]", value = "This is a custom command for <@7852834698300620820>! Try it to see what is does! can use it", inline = False)
     await ctx.send(embed = customEmbed)
+
+@bot.slash_command(description = "This shows all my friend's commands!")
+async def friends(ctx):
+    customEmbed = discord.Embed(title = "Commands", description = "These are the commands I know so far :D", color = discord.Color.blurple())
+    customEmbed.set_thumbnail(url="https://i.imgflip.com/60v6pm.jpg") 
+    customEmbed.add_field(name = "City's Custom Command [-city]", value = "This is a custom command for <@701981194971119690>! Try it to see what is does!", inline = False)
+    customEmbed.add_field(name = "Marchly's Custom Command [-march]", value = "This is a custom command for <@706876255064293416>! Try it to see what is does!", inline = False)
+    customEmbed.add_field(name = "Ghost's Custom Command [-ghost]", value = "This is a custom command for <@606870679543087154>! Try it to see what is does!", inline = False)
+    customEmbed.add_field(name = "Shiv's Custom Command [-shiv]", value = "This is a custom command for <@518744853832794112>! Try it to see what is does!", inline = False)
+    customEmbed.add_field(name = "Stellar's Custom Command [-stellar]", value = "This is a custom command for <@820304171764416582>! Try it to see what is does!!", inline = False)
+    customEmbed.add_field(name = "F O O T [-foot]", value = "F O O T", inline = False)
+    customEmbed.add_field(name = "El Pepe's custom command [-elpepe]", value = "This is a custom command for <@7852834698300620820>! Try it to see what is does! can use it", inline = False)
+    await ctx.respond(embed = customEmbed)
 
 #Edit command
 
@@ -126,6 +149,15 @@ async def serverIp(ctx):
     ipEmbed.add_field(name = "1.19 Vanilla SMP", value = "**IP: OMGLETSGOOOOO.aternos.me**, This is a vanilla 1.19 server! **Cracked is not enabled on this server**", inline = False)
     ipEmbed.set_footer(text = "Ask us if you want any more smps!")
     await ctx.send(embed = ipEmbed)
+
+@bot.slash_command(aliases = ["ip", "server"], description = "This shows all the servers we have!")
+async def serverip(ctx):
+    ipEmbed = discord.Embed(title = "Server IPs", description = "We currently have 2 servers!", color = discord.Color.green())
+    ipEmbed.set_thumbnail(url="https://i.imgflip.com/60v6pm.jpg")
+    ipEmbed.add_field(name = "1.18.2 Modded SMP", value = "**IP: StarCraftReal.aternos.me:27226**. This is an smp with a lot of mods, including Origins, Gravestones, Waystones and more! Check <#971649825630203945> for the modlist! **Cracked is enabled on this server!**", inline = False)
+    ipEmbed.add_field(name = "1.19 Vanilla SMP", value = "**IP: OMGLETSGOOOOO.aternos.me**, This is a vanilla 1.19 server! **Cracked is not enabled on this server**", inline = False)
+    ipEmbed.set_footer(text = "Ask us if you want any more smps!")
+    await ctx.respond(embed = ipEmbed)
 
 #custom commands
 
@@ -173,10 +205,7 @@ async def on_message(msg):
 @bot.event
 async def on_ready():
     activity  = discord.Game(name="!help")
-    await bot.change_presence(activity=discord.Game(name="listening to -help | amogus"))
-    await bot.change_presence(activity=discord.Game(name="minecraft with Defender"))
-    await bot.change_presence(activity=discord.Game(name="among us with citybot"))
-    await bot.change_presence(activity=discord.Game(name="????"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='/help'))
     print("Bot is ready!")
 
 @bot1.event
@@ -186,35 +215,27 @@ async def on_member_join(member):
     dmchannel = await member.create_dm()
     await dmchannel.send(f"Welcome to {guildname}! Read <#971339877004218419> and have fun!")
 
-@bot1.event 
-async def on_raw_reaction_add(payload):
-    emoji = payload.emoji.name
-    member = payload.member
-    message_id = payload.message_id
-    guild_id = payload.guild_id
-    guild = bot.get_guild(guild_id)
-    if emoji == "poggers" and message_id == 985404494642831360:
-        role = discord.utils.get(guild.roles, name = "Minecraft")
-        await member.add_roles(role)
-      
-    if emoji == "amogus" and message_id == 985404494642831360:
-        role = discord.utils.get(guild.roles, name = "Valorant")
-        await member.add_roles(role)
-@bot1.event
-async def on_raw_reaction_remove(payload):
-    user_id = payload.user_id
-    emoji = payload.emoji.name
-    message_id = payload.message_id
-    guild_id = payload.guild_id
-    guild = bot.get_guild(guild_id)
-    member = guild.get_member(user_id)
-    if emoji == "poggers" and message_id == 985404494642831360:
-        role = discord.utils.get(guild.roles, name = "Minecraft")
-        await member.remove_roles(role)
-      
-    if emoji == "amogus" and message_id == 985404494642831360:
-        role = discord.utils.get(guild.roles, name = "Valorant")
-        await member.remove_roles(role)
+# @bot1.event 
+# async def on_raw_reaction_add(payload):
+#     emoji = payload.emoji.name
+#     member = payload.member
+#     message_id = payload.message_id
+#     guild_id = payload.guild_id
+#     guild = bot.get_guild(guild_id)
+#     if emoji == "874902268753903626" and message_id == 987982318541107200:
+#         role = discord.utils.get(guild.roles, name = "bot updates")
+#         await member.remove_roles(role)
+
+# async def on_raw_reaction_remove(payload):
+#     user_id = payload.user_id
+#     emoji = payload.emoji.name
+#     message_id = payload.message_id
+#     guild_id = payload.guild_id
+#     guild = bot.get_guild(guild_id)
+#     member = guild.get_member(user_id)
+#     if emoji == "874902268753903626" and message_id == 987982318541107200:
+#         role = discord.utils.get(guild.roles, name = "bot updates")
+#         await member.remove_roles(role)
 
 #slah commands
 @bot.slash_command(description = "City's Custom Command")
@@ -260,22 +281,25 @@ async def rps(ctx, options : Option(str,"choose your option", choices = ["rock",
     hands = ["rock", "paper", "scissors"]
     opt = options or None
     bothand = random.choice(hands)
-    await ctx.respond(bothand)
+    await ctx.respond("I chose "+ bothand)
     if opt == bothand:
         await ctx.send("Its a Draw! Shall we play again?")
     # elif opt == None:
     #     await ctx.respond("bruh you chose nothing, SO I GUES I WIN HAHA")
     elif opt == "rock":
         if bothand == "paper":
-            await ctx.send("HAH I WON https://tenor.com/view/dancing-cat-dancing-kitten-60fps-boogie-cat-gif-24303276")
+            await ctx.send("HAH I WON")
+            await ctx.send("https://tenor.com/view/dancing-cat-dancing-kitten-60fps-boogie-cat-gif-24303276")
         else:
             await ctx.send("DUDE MY INTERNET WAS LAGGING I SHOULD HAVE WON THA- i mean yay you won haha")
 
     elif opt == "paper":
         if bothand == "rock":
-            await ctx.send("I FRICKING HATE YOU SO MUC- you won https://tenor.com/view/i-hate-you-anakin-star-wars-gif-10358450")
+            await ctx.send("I FRICKING HATE YOU SO MUC- you won")
+            await ctx.send("https://tenor.com/view/i-hate-you-anakin-star-wars-gif-10358450")
         else:
-            await ctx.send("WHEEE I WONNN https://tenor.com/view/dance-dog-dancing-pet-boy-123asd-gif-18609627")
+            await ctx.send("WHEEE I WONNN")
+            await ctx.send("https://tenor.com/view/dance-dog-dancing-pet-boy-123asd-gif-18609627")
 
     elif opt == "scissors":
         if bothand == "paper":
