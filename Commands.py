fa76d11c1ea1ac7844db8426d8a43737f6fdcd18
DefenderYAY <ishu.defender@gmail.com>
2022-07-12 20:45:43 +0530

    fixed /hello

--- Commands.py
+++ Commands.py
@@ -897,8 +897,10 @@ class PollCommand(commands.Cog):
     async def hello(self, ctx, *, member: discord.Member = None):
         member = member or ctx.author
         if self._last_member is None or self._last_member.id != member.id:
+            await ctx.respond("Done!", ephemeral = True)
             await ctx.send(f'Hello {member.name}~')
         else:
+            await ctx.respond("Done!", ephemeral = True)
             await ctx.send(f'Hello {member.name}... This feels familiar.')
         self._last_member = member
 
